838cd55876571834228cff9d109135022a4a8abe
hudson <hudson223@gmail.com>
2021-11-18 19:42:49 -0300

    dashboard updates

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Header, LogoImg, MenuContainer, MenuItemLink, Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import {MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp} from 'react-icons/md';
 
 const Aside: React.FC = () => {
   return (
     <Container>
-        <h1>Aside</h1>
+        <Header>
+          <LogoImg src={logoImg} alt="Logo minha carteira" />
+          <Title>Minha Carteira</Title>
+        </Header>
+
+        <MenuContainer>
+          <MenuItemLink href="#"><MdDashboard/>Dashboard</MenuItemLink>
+          <MenuItemLink href="#"><MdArrowDownward />Entradas</MenuItemLink>
+          <MenuItemLink href="#"><MdArrowUpward />Saídas</MenuItemLink>
+          <MenuItemLink href="#"><MdExitToApp />Sair</MenuItemLink>
+        </MenuContainer>
     </Container>
   );
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -4,4 +4,47 @@ export const Container = styled.div`
     grid-area: AS;
     color: ${props => props.theme.color.white};
     background-color: ${props => props.theme.color.secondary};
+    padding-left: 20px;
+    border-right: 1px solid ${props => props.theme.color.gray};
 `;
+
+export const Header = styled.header`
+    height: 70px;
+    display: flex;
+    align-items: center;
+`;
+
+export const LogoImg = styled.img`
+    height: 40px;
+    width: 40px;
+`;
+
+export const Title= styled.h3`
+    color: ${props => props.theme.color.white};
+    margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const MenuItemLink= styled.a`
+    display: flex;
+    align-items: center;
+    color: ${props => props.theme.color.info};
+    text-decoration: none;
+    transition: opacity .3s;
+    &:not(:last-child) {
+        margin-bottom: 15px;
+    }
+    &:hover {
+        opacity: .7;
+    }
+    > svg {
+        font-size: 18px;
+        margin-right: 5px;
+    }
+`;
+
