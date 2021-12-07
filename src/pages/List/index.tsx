72106424bcd22b4c0dce03a26af70370be678d22
hudson <hudson223@gmail.com>
2021-12-06 21:47:22 -0300

    filter updates

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo, useState, useEffect} from 'react';
+import React, { useMemo, useState, useEffect, MouseEventHandler} from 'react';
+import { v4 as uuid_v4 } from 'uuid';
 import {Container, Content, Filters} from './styles';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
@@ -8,6 +9,7 @@ import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
 import formatCurrency from '../../utils/formatCurrency';
 import formatDate from '../../utils/formatDate';
+import listOfMonths from '../../utils/months';
 
 interface IData {
     id: string,
@@ -23,6 +25,7 @@ const List: React.FC = () => {
     const [data, setData] = useState<IData[]>([]);
     const [monthSelected, setMonthSelected] = useState<string>(String(new Date().getMonth() + 1));
     const [yearSelected, setYearSelected] = useState<string>(String(new Date().getFullYear()));
+    const [selectedFrequency, setSelectedFrequency] = useState(['recorrente', 'eventual']);
 
     const {type} = useParams();
 
@@ -40,29 +43,45 @@ const List: React.FC = () => {
         return type === 'entry-balance'? gains : expenses
     }, [type]);
 
-    const months = [
-        {value: 1, label: 'Janeiro'},
-        {value: 2, label: 'Fevereiro'},
-        {value: 3, label: 'Março'},
-        {value: 4, label: 'Abril'},
-        {value: 5, label: 'Maio'},
-        {value: 6, label: 'Junho'},
-        {value: 7, label: 'Julho'},
-        {value: 8, label: 'Agosto'},
-        {value: 9, label: 'Setembro'},
-        {value: 10, label: 'Outubro'},
-        {value: 11, label: 'Novembro'},
-        {value: 12, label: 'Dezembro'},
-    ];
+    const months = useMemo(() => {
+        return listOfMonths.map((month, index) => {
+            return {
+                value: index + 1,
+                label: month
+            }
+        })
+    }, []);
 
-    const years = [
-        {value: 2023, label: 2023},
-        {value: 2022, label: 2022},
-        {value: 2021, label: 2021},
-        {value: 2020, label: 2020},
-        {value: 2019, label: 2019},
-        {value: 2018, label: 2018},
-    ];
+    const years = useMemo(() => {
+        let uniqueYears: number[] = [];
+
+        listData.forEach(item => {
+            const date = new Date(item.date)
+            const year = date.getFullYear()
+
+            if(!uniqueYears.includes(year)) {
+                uniqueYears.push(year)
+            }
+        });
+
+        return uniqueYears.map( year => {
+            return {
+                value: year,
+                label: year,
+            }
+        })
+    }, [listData]);
+
+    const handleFrequencyClick = (frequency: string) => {
+        const alreadySelected = selectedFrequency.findIndex(item => item === frequency)
+
+        if(alreadySelected >= 0) {
+            const filtered = selectedFrequency.filter(item => item !== frequency)
+            setSelectedFrequency(filtered)
+        } else {
+            setSelectedFrequency((prev) => [...prev, frequency])
+        }
+    }
 
     useEffect(() => {
 
@@ -72,14 +91,13 @@ const List: React.FC = () => {
             const month = String(date.getMonth() + 1);
             const year = String(date.getFullYear());
 
-            return month === monthSelected && year === yearSelected;
+            return month === monthSelected && year === yearSelected && selectedFrequency.includes(item.frequency);
         });
 
-
-        const formattedData = filteredDate.map((item, i) => {
+        const formattedData = filteredDate.map((item) => {
 
             return {
-                id: String(i),
+                id: uuid_v4(),
                 description: item.description,
                 amountFormatted: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
@@ -89,7 +107,7 @@ const List: React.FC = () => {
         })
 
         setData(formattedData)
-    }, [listData, monthSelected, yearSelected, data.length]);
+    }, [listData, monthSelected, yearSelected, data.length, selectedFrequency]);
 
     return (
         <Container>
@@ -99,14 +117,26 @@ const List: React.FC = () => {
             </ContentHeader>
 
             <Filters>
-                <button type="button" className="tag-filter tag-filter-recurrent">Recorrentes</button>
-                <button type="button" className="tag-filter tag-filter-eventual">Eventuais</button>
+                <button 
+                    type="button" 
+                    className={`tag-filter tag-filter-recurrent ${selectedFrequency.includes('recorrente') && 'tag-actived'}`}
+                    onClick={() => handleFrequencyClick('recorrente')}
+                >
+                        Recorrentes
+                </button>
+                <button 
+                    type="button" 
+                    className={`tag-filter tag-filter-eventual ${selectedFrequency.includes('eventual') && 'tag-actived'}`}
+                    onClick={() => handleFrequencyClick('eventual')}
+                >
+                        Eventuais
+                </button>
             </Filters>
 
             <Content>
                 {data && data.map((item) => (
                     <HistoryFinanceCard
-                        key={item.id}
+                        key={uuid_v4()}
                         tagColor={item.tagColor}
                         title={item.description}
                         subtitle={item.dateFormatted}
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -17,6 +17,7 @@ export const Filters = styled.div`
         background: none;
         color: ${props => props.theme.color.white};
         transition: opacity .3s;
+        opacity: .4;
         &:not(:last-child) {
             margin-right: 10px;
         }
@@ -35,6 +36,9 @@ export const Filters = styled.div`
         }
         &-eventual::after {
             border-bottom: 10px solid ${props => props.theme.color.warning};
+        }
+        &.tag-actived {
+            opacity: 1;
         }
         
     }
