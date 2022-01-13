85d487968b0b1059c1d87e64b2ada231534aef01
Hudson Henrique de Freitas <hudson@MINERACAOCAFE.local>
2022-01-13 08:17:37 -0300

    fix: BarChartBox legend fix

--- src/components/BarChartBox/index.tsx
+++ src/components/BarChartBox/index.tsx
@@ -33,7 +33,7 @@ const BarChartBox: React.FC<IBarChartProps> = ({title, data}) => {
         <SideRight>
             <ResponsiveContainer>
                 <BarChart data={data}>
-                    <Bar dataKey="amount">
+                    <Bar dataKey="amount" name="Valor">
                         {data.map((indicator) => (
                             <Cell 
                                 key={indicator.name}
@@ -43,7 +43,7 @@ const BarChartBox: React.FC<IBarChartProps> = ({title, data}) => {
                             />
                         ))}
                     </Bar>
-                    <Tooltip formatter={(value: number) => formatCurrency(Number(value))} />
+                    <Tooltip cursor={{fill: 'none'}} formatter={(value: number) => formatCurrency(Number(value))} />
                 </BarChart>
             </ResponsiveContainer>
         </SideRight>
